a05fea13d5e778e649b898c33f85f59e3b931933
Kyle Wilcox <kyle@axiomdatascience.com>
2015-11-03 14:46:12 -0500

    Catch case when unit_metadata is null and don't error out
    Former-commit-id: 96ed89a36a6d159c8cc6941bfca9883bd551fadf

--- src/main/java/gov/noaa/pfel/erddap/dataset/EDDTableFromAxiomSensorCSVService.java
+++ src/main/java/gov/noaa/pfel/erddap/dataset/EDDTableFromAxiomSensorCSVService.java
@@ -293,10 +293,12 @@ public class EDDTableFromAxiomSensorCSVService extends EDDTableFromAsciiService 
                     all_parameter_info.add(name_unit);
                 } else {
                     ArrayList<String[]> unit_metadata = parameter_type_lookup.get(param.getInt("idParameterType"));
-                    for (String[] unit_array : unit_metadata) {
-                        name_unit[1] = unit_array[0];
-                        name_unit[2] = unit_array[1];
-                        all_parameter_info.add(name_unit.clone());
+                    if (!unit_metadata.equals(null)) {
+                        for (String[] unit_array : unit_metadata) {
+                            name_unit[1] = unit_array[0];
+                            name_unit[2] = unit_array[1];
+                            all_parameter_info.add(name_unit.clone());
+                        }
                     }
                 }
                 parameter_lookup.put(param.getInt("id"), all_parameter_info);
